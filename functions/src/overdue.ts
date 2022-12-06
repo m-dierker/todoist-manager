d3c2f54d4b1da4725311a8d27e1c3599b2cfb321
Matthew Dierker <mvd7793@gmail.com>
2022-12-06 00:30:29 -0800

    fix a utc bug

--- functions/src/overdue.ts
+++ functions/src/overdue.ts
@@ -12,9 +12,18 @@ export class OverdueTaskRescheduler extends BaseRescheduler {
     now: moment.Moment;
   }): Promise<moment.Moment | undefined> {
     const startOfCurrentDay = now.startOf("day");
-    const taskDueDate = moment
-      .tz(task.due!.date, process.env.TIMEZONE!)
-      .startOf("day");
+    console.log(task.content, task.due);
+    let taskDueDate;
+    if (task.due?.datetime) {
+      // This has to be done manually since times are stored in UTC,
+      // so a task due late in the day might be due "tomorrow" in UTC.
+      // Both due.date and due.datetime are in UTC.
+      taskDueDate = moment.tz(task.due!.datetime, process.env.TIMEZONE!);
+    } else {
+      taskDueDate = moment
+        .tz(task.due!.date, process.env.TIMEZONE!)
+        .startOf("day");
+    }
     // If a task isn't due yet, no adjustment is needed.
     if (!taskDueDate.isBefore(startOfCurrentDay)) {
       return undefined;
@@ -23,7 +32,7 @@ export class OverdueTaskRescheduler extends BaseRescheduler {
     // If datetime is set, the task has a specific due time.
     // Adjust the day while keeping that time.
     if (task.due?.datetime) {
-      const taskDatetime = moment.tz(task.due!.datetime, process.env.TIMEZONE!);
+      const taskDatetime = moment.tz(task.due.datetime, process.env.TIMEZONE!);
       const todaySameTime = moment(startOfCurrentDay)
         .hour(taskDatetime.hour())
         .minute(taskDatetime.minute())
